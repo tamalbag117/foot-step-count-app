cd5758dde8530d7f5b041b87b26b48589dd260f7
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2022-06-30 12:22:51 +0530

    secondary changes

--- start/src/main/java/com/example/wear/tiles/GoalsTileService.kt
+++ start/src/main/java/com/example/wear/tiles/GoalsTileService.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.wear.tiles
 
 import androidx.core.content.ContextCompat
@@ -51,10 +37,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.guava.future
 
-// TODO: Review Constants.
-// Updating this version triggers a new call to onResourcesRequest(). This is useful for dynamic
-// resources, the contents of which change even though their id stays the same (e.g. a graph).
-// In this sample, our resources are all fixed, so we use a constant value.
 private const val RESOURCES_VERSION = "1"
 
 // dimensions
@@ -64,31 +46,27 @@ private val BUTTON_RADIUS = dp(24f)
 private val BUTTON_PADDING = dp(12f)
 private val VERTICAL_SPACING_HEIGHT = dp(8f)
 
-// Complete degrees for a circle (relates to [Arc] component)
 private const val ARC_TOTAL_DEGREES = 360f
 
 // identifiers
 private const val ID_IMAGE_START_RUN = "image_start_run"
 private const val ID_CLICK_START_RUN = "click_start_run"
 
-/**
- * Creates a Fitness Tile, showing your progress towards a daily goal. The progress is defined
- * randomly, for demo purposes only. A new random progress is shown when the user taps the button.
- */
-class GoalsTileService : TileService() {
-    // For coroutines, use a custom scope we can cancel when the service is destroyed
-    private val serviceScope = CoroutineScope(Dispatchers.IO)
 
-    // TODO: Build a Tile.
+class GoalsTileService : TileService() {
+        private val serviceScope = CoroutineScope(Dispatchers.IO)
+
     override fun onTileRequest(requestParams: TileRequest) = serviceScope.future {
+
+        val goalProgress = GoalsRepository.getGoalProgress()
+    
+        val deviceParams = requestParams.deviceParameters!!
 
         // Creates Tile.
         Tile.Builder()
-            // If there are any graphics/images defined in the Tile's layout, the system will
-            // retrieve them via onResourcesRequest() and match them with this version number.
             .setResourcesVersion(RESOURCES_VERSION)
 
-            // Creates a timeline to hold one or more tile entries for a specific time periods.
+           
             .setTimeline(
                 Timeline.Builder()
                     .addTimelineEntry(
@@ -96,11 +74,7 @@ class GoalsTileService : TileService() {
                             .setLayout(
                                 Layout.Builder()
                                     .setRoot(
-                                        Text.Builder()
-                                            .setText(
-                                                getString(R.string.placeholder_text)
-                                            )
-                                            .build()
+                                        layout(goalProgress, deviceParams)
                                     )
                                     .build()
                             )
@@ -110,31 +84,117 @@ class GoalsTileService : TileService() {
             ).build()
     }
 
-    // TODO: Supply resources (graphics) for the Tile.
+    //  Supply resources (graphics) for the Tile.
     override fun onResourcesRequest(requestParams: ResourcesRequest) = serviceScope.future {
         Resources.Builder()
             .setVersion(RESOURCES_VERSION)
-            // No Resources quite yet!
+            .addIdToImageMapping(
+                ID_IMAGE_START_RUN,
+                ImageResource.Builder()
+                    .setAndroidResourceByResId(
+                        AndroidImageResourceByResId.Builder()
+                            .setResourceId(R.drawable.ic_run)
+                            .build()
+                    )
+                    .build()
+            )
             .build()
     }
 
-    // TODO: Review onDestroy() - cancellation of the serviceScope
+ 
     override fun onDestroy() {
         super.onDestroy()
         // Cleans up the coroutine
         serviceScope.cancel()
     }
 
-    // TODO: Create root Box layout and content.
+    // Create root Box layout and content.
 
+    private fun layout(goalProgress: GoalProgress, deviceParameters: DeviceParameters) =
+        Box.Builder()
+            // Sets width and height to expand and take up entire Tile space.
+            .setWidth(expand())
+            .setHeight(expand())
 
-    // TODO: Create a function that constructs an Arc representation of the current step progress.
+            // Adds an [Arc] via local function.
+            .addContent(progressArc(goalProgress.percentage))
 
+    
+            .addContent(
+                Column.Builder()
+                    // Adds a [Text] via local function.
+                    .addContent(
+                        currentStepsText(goalProgress.current.toString(), deviceParameters)
+                    )
+                    // Adds a [Text] via local function.
+                    .addContent(
+                        totalStepsText(
+                            resources.getString(R.string.goal, goalProgress.goal),
+                            deviceParameters
+                        )
+                    )
+                  
+                    .addContent(Spacer.Builder().setHeight(VERTICAL_SPACING_HEIGHT).build())
+                    // Adds an [Image] via local function.
+                    .addContent(startRunButton())
+                    .build()
+            )
+            .build()
+            
+    private fun progressArc(percentage: Float) = Arc.Builder()
+        .addContent(
+            ArcLine.Builder()
+                .setLength(degrees(percentage * ARC_TOTAL_DEGREES))
+                .setColor(argb(ContextCompat.getColor(this, R.color.primary)))
+                .setThickness(PROGRESS_BAR_THICKNESS)
+                .build()
+        )
+        // Element will start at 12 o'clock or 0 degree position in the circle.
+        .setAnchorAngle(degrees(0.0f))
+        .setAnchorType(ARC_ANCHOR_START)
+        .build()
+    //  construct/stylize Text representations of the step count & goal.
+  
+    private fun currentStepsText(current: String, deviceParameters: DeviceParameters) = Text.Builder()
+        .setText(current)
+        .setFontStyle(FontStyles.display2(deviceParameters).build())
+        .build()
 
-    // TODO: Create functions that construct/stylize Text representations of the step count & goal.
+    // Creates a [Text] with total step count goal and stylizes it.
+    private fun totalStepsText(goal: String, deviceParameters: DeviceParameters) = Text.Builder()
+        .setText(goal)
+        .setFontStyle(FontStyles.title3(deviceParameters).build())
+        .build()
 
-
-    // TODO: Create a function that constructs/stylizes a clickable Image of a running icon.
-
+    private fun startRunButton() =
+        Image.Builder()
+            .setWidth(BUTTON_SIZE)
+            .setHeight(BUTTON_SIZE)
+            .setResourceId(ID_IMAGE_START_RUN)
+            .setModifiers(
+                Modifiers.Builder()
+                    .setPadding(
+                        Padding.Builder()
+                            .setStart(BUTTON_PADDING)
+                            .setEnd(BUTTON_PADDING)
+                            .setTop(BUTTON_PADDING)
+                            .setBottom(BUTTON_PADDING)
+                            .build()
+                    )
+                    .setBackground(
+                        Background.Builder()
+                            .setCorner(Corner.Builder().setRadius(BUTTON_RADIUS).build())
+                            .setColor(argb(ContextCompat.getColor(this, R.color.primaryDark)))
+                            .build()
+                    )
+                    .setClickable(
+                        Clickable.Builder()
+                            .setId(ID_CLICK_START_RUN)
+                            .setOnClick(ActionBuilders.LoadAction.Builder().build())
+                            .build()
+                    )
+                    .build()
+            )
+            .build()
 
 }
